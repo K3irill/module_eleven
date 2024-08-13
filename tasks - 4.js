7de7f3ee271877c369d46945aaa555bffe86653e
K3rill <razdor.2019@mail.ru>
2024-08-13 18:34:39 +0300

    tasks - 4.js

--- tasks - 4.js
+++ tasks - 4.js
@@ -1,1 +1,12 @@
-'use strict'
+"use strict";
+
+function intervalID(a, b) {
+    
+ let i = setInterval(() => {
+    console.log(a++);
+    if (a > b) clearInterval(i);
+    
+}, 1000)
+}
+intervalID(5,15)
+
