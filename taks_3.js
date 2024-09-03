2d945abff8aa9e569b834c9b6a535bed4b7723fb
K3rill <razdor.2019@mail.ru>
2024-09-03 15:55:58 +0300

    task three

---
+++ taks_3.js
@@ -0,0 +1,52 @@
+const input = document.getElementById("itext");
+const button = document.getElementById("button");
+
+function checkFun(fun) {
+  if (input.value < 1 || input.value > 10) {
+    return alert(`«число вне диапазона от 1 до 10».`);
+  } else {
+    fun("https://jsonplaceholder.typicode.com/photos?_limit=8", displayResults);
+  }
+}
+
+function callRequest(url, callback) {
+  let requset = new XMLHttpRequest();
+  requset.open("GET", url, true);
+  requset.onload = function () {
+    if (requset.status != 200) {
+      console.log(`Status: ${requset.status}`);
+    } else {
+      const result = JSON.parse(requset.response);
+      if (callback) {
+        callback(result);
+      }
+    }
+  };
+  requset.onerror = function () {
+    console.log("Ошибка! Статус ответа: ", requset.status);
+  };
+  requset.send();
+}
+
+const resultsBlock = document.querySelector(".results");
+
+function displayResults(value) {
+  let cards = "";
+  value.forEach((el) => {
+    const cardBlock = `
+      <div class="card">
+        <img
+          src="${el.thumbnailUrl}"
+          class="card-image"
+        />
+        <p>${el.title}</p>
+      </div>
+    `;
+    cards = cards + cardBlock;
+  });
+  resultsBlock.innerHTML = cards;
+}
+
+button.addEventListener("click", () => {
+  checkFun(callRequest);
+});
