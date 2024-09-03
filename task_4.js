a6bd34504dbc8129e3c87d7be3dbe09d032295c9
K3rill <razdor.2019@mail.ru>
2024-09-03 21:19:31 +0300

    task four

---
+++ task_4.js
@@ -0,0 +1,30 @@
+const button = document.querySelector("button");
+const resultBlock = document.querySelector(".results");
+const values = document.querySelectorAll("#itext");
+
+button.addEventListener("click", requset);
+
+function requset() {
+  const values = document.querySelectorAll("#itext");
+  let width = values[0].value;
+  let height = values[1].value;
+  if ((width < 100 || width > 300) || (height < 100 || height > 300)) {
+    return console.log("erroooooor");
+  } else {
+    let url = `https://dummyimage.com/${values[0].value}x${values[1].value}/`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("not ok");
+        }
+        resultBlock.innerHTML = `
+              <img src="${url}"/>
+        `;
+        console.log("yeees");
+      })
+      .catch((err) => {
+        resultBlock.innerHTML = `Erroooooor: ${err.message}`;
+        console.log(err);
+      });
+  }
+}
